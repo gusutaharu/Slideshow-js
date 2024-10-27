15172d957ebe0987ea923e2b3602a73696483cd7
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-10-28 08:32:52 +0900

    lesson12

--- example/example.js
+++ example/example.js
@@ -3,7 +3,24 @@
   const thumbnails = document.querySelectorAll('.thumbnail');
   const mainImage = document.getElementById('main-img');
 
+  thumbnails[0].addEventListener('click', ()=>{
+    mainImage.src = thumbnails[0].src;
+    thumbnails[1].classList.remove('active');
+    thumbnails[2].classList.remove('active');
+    thumbnails[0].classList.add('active');
+  });
+
   thumbnails[1].addEventListener('click', ()=>{
     mainImage.src = thumbnails[1].src;
+    thumbnails[0].classList.remove('active');
+    thumbnails[2].classList.remove('active');
+    thumbnails[1].classList.add('active');
+  });
+
+  thumbnails[2].addEventListener('click', ()=>{
+    mainImage.src = thumbnails[2].src;
+    thumbnails[0].classList.remove('active');
+    thumbnails[1].classList.remove('active');
+    thumbnails[2].classList.add('active');
   });
 }
