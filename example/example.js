5c676fa90ad30c470a5a3e76ef9f93102dcf984c
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-10-28 08:30:26 +0900

    lesson11

--- example/example.js
+++ example/example.js
@@ -1,8 +1,9 @@
 'use strict'
 {
   const thumbnails = document.querySelectorAll('.thumbnail');
+  const mainImage = document.getElementById('main-img');
 
   thumbnails[1].addEventListener('click', ()=>{
-    
-  })
+    mainImage.src = thumbnails[1].src;
+  });
 }
